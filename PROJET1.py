54ff54e18d6d4e003120a4310454ca2472eeb667
bixente-r <80693578+bixente-r@users.noreply.github.com>
2022-01-06 15:07:31 +0100

    Add files via upload

---
+++ PROJET1.py
@@ -0,0 +1,104 @@
+"""
+Fichier de test du projet Ma313
+
+Ne lancer qu'une partie à la fois (mettre entre """ """ les autres parties) pour ne pas saturer la mémoire
+
+Auteur : Maxime Gosselin
+Classe : 3PF2
+Groupe : Maxime Gosselin - Jimmy Hoarau - Matthieu Janiaut - Antonin Maitre
+"""
+
+import lib_projet as lib
+import  numpy as np
+
+A1 = np.array([[3, 1, 1], [1, 3, 1], [1, 1, 3]])
+A2 = np.array([[3, 0, 1], [0, 7, 0], [1, 0, 3]])
+Msdp = lib.gen_matrice_sdp()
+Hil3 = lib.Hilbert(3)
+Hil10 = lib.Hilbert(10)
+Ms = lib.gen_matrice_sym()
+M = lib.gen_matrice()
+
+
+###### Méthode de la puissane itérée ######
+print("Matrice A1 sujet")
+lib.affichage_méthode1(A1)
+lib.affichage_norme1(A1)
+print("####################################")
+print("Matrice A2 sujet")
+lib.affichage_méthode1(A2)
+lib.affichage_norme1(A2)
+print("####################################")
+print("Matrice symétrique définie positive aléatoire")
+lib.affichage_méthode1(Msdp[0])
+lib.affichage_norme1(Msdp[0])
+print("####################################")
+print("Matrice symétrique définie positive aléatoire")
+lib.affichage_méthode1(Msdp[1])
+lib.affichage_norme1(Msdp[1])
+print("####################################")
+print("Matrice de Hilbert n=3")
+lib.affichage_méthode1(Hil3)
+lib.affichage_norme1(Hil3)
+print("####################################")
+print("Matrice de Hilbert n=10")
+lib.affichage_méthode1(Hil10)
+lib.affichage_norme1(Hil10)
+
+
+###### Méthode de la puissane inverse ######
+
+print("Matrice A1 sujet")
+lib.affichage_méthode2(A1)
+lib.affichage_cond2(A1)
+print("####################################")
+print("Matrice A2 sujet")
+lib.affichage_méthode2(A2)
+lib.affichage_cond2(A2)
+print("####################################")
+print("Matrice symétrique définie positive aléatoire")
+lib.affichage_méthode2(Msdp[0])
+lib.affichage_cond2(Msdp[0])
+print("####################################")
+print("Matrice symétrique définie positive aléatoire")
+lib.affichage_méthode2(Msdp[1])
+lib.affichage_cond2(Msdp[1])
+print("####################################")
+print("Matrice de Hilbert n=3")
+lib.affichage_méthode2(Hil3)
+lib.affichage_cond2(Hil3)
+print("####################################")
+print("Matrice de Hilbert n=10")
+lib.affichage_méthode2(Hil10)
+lib.affichage_cond2(Hil10)
+
+
+###### Adaptation au cas d'une matrice quelconque ######
+
+A3 = np.array([[1, 1], [1, -2]])
+A4 = np.array([[0, 1], [1, 0]])
+
+print("Matrice A3 sujet")
+lib.affichage_méthode3(A3)
+print("Matrice A4 sujet")
+lib.affichage_méthode3(A4)
+
+
+###### Introduction à la méthode de diagonalisation par QR ######
+print("Matrice A1 sujet (symétrique définie positive)")
+lib.affichage_méthode4(A1)
+print("####################################")
+print("Matrice symétrique définie positive aléatoire")
+lib.affichage_méthode4(Msdp[0])
+print("####################################")
+print("Matrice symétrique aléatoire")
+lib.affichage_méthode4(Ms[0])
+print("####################################")
+print("Matrice symétrique aléatoire")
+lib.affichage_méthode4(Ms[1])
+print("####################################")
+print("Matrice aléatoire")
+lib.affichage_méthode4(M[0])
+print("####################################")
+print("Matrice aléatoire")
+lib.affichage_méthode4(M[1])
---
+++ lib_projet.py
@@ -0,0 +1,395 @@
+"""
+Librairie des fonctions du projet Ma313
+
+Auteur : Maxime Gosselin
+Classe : 3PF2
+Groupe : Maxime Gosselin - Jimmy Hoarau - Matthieu Janiaut - Antonin Maitre
+"""
+import numpy as np
+
+
+def matrice_def_pos(A):
+    """
+    Vérifie qu'une matrice est définie positive
+    Renvoie booléen
+    """
+    if np.all(np.linalg.eigvals(A) > 0):
+        mdp = True
+    else:
+        print("la matrice n'est pas défninie positive")
+        mdp = False
+    return mdp
+
+
+def matrice_sym(A):
+    """
+    Vérifie qu'une matrice est symétrique
+    Renvoie booléen
+    """
+    At = np.transpose(A)
+    # print(A)
+     #print(At)
+    if (np.allclose(A, A.T) == True):
+        ms = True
+    else:
+        print("la matrice n'est pas symétrique")
+        ms = False
+    return ms
+
+
+
+
+###### Méthode de la puissance itérée ######
+
+def méthode_puissance_itérée(A, epsilon=10**-10, Nitermax=250):
+    """
+    Applique la méthode de la puissance itérée à une matrice
+
+    Paramètres : Matrice carré - epsilon float : << 1 - Nombre max d'itération
+
+    Sortie : 
+        - Approximation du vecteur propre associé
+        - Approximation de la valeur propre
+        - Nombre d'itérations
+        - Dernier écart calculé
+    """
+    mdp = matrice_def_pos(A)
+    ms = matrice_sym(A)
+    #At = np.transpose(A)
+    #a = np.dot(At,A)
+    if (mdp != True or ms != True):
+        pass
+    else:
+        n,n = np.shape(A)
+        x0 = np.zeros((n,1))
+        x0[:,0] = 1
+        x = x0
+        Niter = 0
+        #print(np.linalg.norm(x), "\n")
+        w0 = (1/np.linalg.norm(x))*x
+        w = w0
+        err = epsilon + 1
+        norm_c = list()
+        list_w = list()
+
+        while(Nitermax > Niter and err > epsilon):
+            wk = w
+            #print("wk = ", wk, "\n")
+            c = np.dot(A,wk)
+            #print("norm c = ", np.linalg.norm(c))
+            #print("c = ", c,"\n")
+            w = (1/np.linalg.norm(c))*c
+            #print("wk+1 = ",w,"\n")
+            Niter += 1
+            err = np.linalg.norm(w-wk)
+            #print(Niter,err,"---------------------","\n")
+            list_w.append(w)
+            norm_c.append(np.linalg.norm(c))
+    return list_w[-1], norm_c[-1], Niter, err
+
+
+def norme_euclidienne(A):
+    """
+    Calcule la norme euclidienne d'une matrice carré à partir de la méthode de la puissance itérée
+    
+    Paramètre : Matrice carré
+
+    Sortie :
+        - Norme calculée à partir de la puissance itérée
+        - Norme euclidienne de numpy
+    """
+    At = np.transpose(A)
+    a = np.dot(At,A)
+    vp_max = méthode_puissance_itérée(a, 10**-10, 50)[1]
+    norm_A_calc = np.sqrt(vp_max)
+    norm_a_np = np.linalg.norm(A,2)
+    return norm_A_calc, norm_a_np
+
+
+
+
+
+###### Méthode de la puissance inverse ######
+
+def méthode_puissance_inverse(A, epsilon=10**-10, Nitermax=250):
+    """
+    Applique la méthode de la puissance inverse à une matrice
+
+    Paramètres : Matrice carré - epsilon : float << 1 - Nombre max d'itération
+
+    Sortie : 
+        - Approximation du vecteur propre associé
+        - Approximation de la plus grande valeur propre de la matrice inverse
+        - Approximation de la plus petite valeur propre de la matrice
+        - Nombre d'itérations
+        - Dernier écart calculé
+    """
+    mdp = matrice_def_pos(A)
+    ms = matrice_sym(A)
+    a = np.linalg.inv(A)
+    if (mdp != True or ms != True):
+        print("")
+    else:
+        n,n = np.shape(A)
+        x0 = np.zeros((n,1))
+        x0[:,0] = 1
+        x = x0
+        Niter = 0
+        #print(np.linalg.norm(x), "\n")
+        w0 = (1/np.linalg.norm(x))*x
+        w = w0
+        err = epsilon + 1
+        norm_c = list()
+        list_w = list()
+
+        while(Nitermax > Niter and err > epsilon):
+            wk = w
+            #print("wk = ", wk, "\n")
+            c = np.dot(a,wk)
+            #print("norm c = ", np.linalg.norm(c))
+            #print("c = ", c,"\n")
+            w = (1/np.linalg.norm(c))*c
+            #print("wk+1 = ",w,"\n")
+            Niter += 1
+            err = np.linalg.norm(w-wk)
+            #print(Niter,err,"---------------------","\n")
+            list_w.append(w)
+            norm_c.append(np.linalg.norm(c))
+            vp = 1/norm_c[-1]
+    return list_w[-1], norm_c[-1], vp, Niter, err
+
+
+def norme_euclidienne_2(A):
+    """
+    Calcule la norme euclidienne de l'inverse d'une matrice carré à partir de la méthode de la puissance inverse
+        
+    Paramètre : Matrice carré
+
+    Sortie :
+        - Norme calculée à partir de la puissance inverse
+        - Norme euclidienne de numpy
+    """
+    At = np.transpose(A)
+    a = np.dot(At,A)
+    vp_max = méthode_puissance_inverse(a, 10**-10, 50)[1]
+    norm_A_calc = np.sqrt(vp_max)
+    norm_a_np = np.linalg.norm(np.linalg.inv(A),2)
+
+    return norm_A_calc, norm_a_np
+
+
+def conditionnement(A):
+    """
+    Calcule le conditionnement d'une matrice carré à partir de la méthode de la puissance itérée et inverse
+        
+    Paramètre : Matrice carré
+
+    Sortie :
+        - Conditionnement calculé à partir des méthodes
+        - Conditionnement de numpy
+    """
+    norm_A = norme_euclidienne(A)[0]
+    norm_inv_A = norme_euclidienne_2(A)[0]
+    cond = norm_inv_A * norm_A
+    condd = np.linalg.cond(A)
+    return cond, condd
+
+###### Adaptation au cas d'une matrice quelconque ######
+
+def méthode_puissance_itérée2(A, epsilon=10**-10, Nitermax=250):
+    """
+    Applique la méthode de la puissance itérée à une matrice quelconque
+
+    Paramètres : Matrice carré - epsilon float : << 1 - Nombre max d'itération
+
+    Sortie : 
+        - Approximation du vecteur propre associé
+        - Approximation de la valeur propre
+        - Nombre d'itérations
+        - Dernier écart calculé
+    """
+    mdp = matrice_def_pos(A)
+    ms = matrice_sym(A)
+    print("\n")
+    n,n = np.shape(A)
+    x0 = np.zeros((n,1))
+    x0[:,0] = 1
+    x = x0
+    Niter = 0
+    #print(np.linalg.norm(x), "\n")
+    w0 = (1/np.linalg.norm(x))*x
+    w = w0
+    w2 = w0
+    err = epsilon + 1
+    err2 = epsilon + 1
+    norm_c = list()
+    list_w = list()
+    norm_c2 = list()
+    list_w2 = list()
+
+    while(Nitermax > Niter and (err > epsilon or err2 > epsilon)):
+        wk = w #((-1)**Niter)*
+        wk2 = ((-1)**Niter)*w2
+        # print("wk = ", wk, "\n")
+        c = np.dot(A,wk)
+        c2 = np.dot(A,wk2)
+        #print("norm c = ", np.linalg.norm(c))
+        #print("c = ", c,"\n")
+        w = (1/np.linalg.norm(c))*c
+        w2 = (1/np.linalg.norm(c2))*c2
+        #print("wk+1 = ",w,"\n")
+        Niter += 1
+        err = np.linalg.norm(w-wk)
+        err2 = np.linalg.norm(w2-wk2)
+        #print(Niter,err,"---------------------","\n")
+        list_w.append(w)
+        norm_c.append(np.linalg.norm(c))
+        list_w2.append(w2)
+        norm_c2.append(np.linalg.norm(c2))
+    if err < epsilon:
+        sortie = list_w[-1], norm_c[-1], Niter, err, str(1), 
+    else:
+        sortie = list_w2[-1], norm_c2[-1], Niter, err2, str(2),        
+    return sortie
+
+
+
+###### Introduction à la méthode de diagonalisation par QR ######
+
+def diag_QR(A, epsilon=10**-10, Nitermax=250):
+    """
+    Applique la méthode de diagonalisation par QR
+
+    Paramètres : Matrice carré - epsilon : float << 1 - Nombre max d'itération
+
+    Sortie : 
+        - Dernière matrice Ak calculée
+        - Matrice de passage orthogonale P telle que Ak = Pt*A*P
+        - Nombre d'itérations
+        - Valeur maximale Mk des valeurs absolues des coefficients sous la diagonale de Ak
+    """
+    n,n = np.shape(A)
+    Niter = 0
+    Mk = epsilon + 1
+    A0 = A
+    a = A0
+    list_Q = []
+    while (Nitermax > Niter and Mk > epsilon):
+        ak = a
+        # print("ak", ak,"\n")
+        Qk = np.linalg.qr(ak)[0]
+        Rk = np.linalg.qr(ak)[1]
+        list_Q.append(Qk)
+        
+        m = np.tril(ak)-np.diag(np.diag(ak))
+        M = abs(m)
+        Mk = M.max() 
+        Niter += 1
+        a = np.dot(Rk,Qk)
+        # print("a", a, "\n")
+    return ak, list_Q[0], Niter, Mk
+
+
+
+
+
+###### Génération de matrices ######
+
+def gen_matrice_sdp(min=2, max=10, pas=5):
+    """
+    Génère une liste de matrice symétrique définie positive
+    """
+    list_Msdp = []
+    for i in range(min, max, pas):
+        A = np.random.rand(i,i)
+        As = (A + A.T)/2
+        # print(As,"\n")
+        At = np.transpose(As)
+        # print(At,"\n")
+        Asdp = np.dot(As, At)
+        # print(Asdp)
+        dp = matrice_def_pos(Asdp)
+        if dp == True:
+            list_Msdp.append(Asdp)
+    return list_Msdp
+
+
+def Hilbert(n):
+    """
+    Renvoie la matrice de HIlbert de taille n
+    """
+    H = np.zeros((n, n))
+    for i in range(0,n):
+        for j in range(0, n):
+            H[i,j] = 1.0/(i+j+1)
+    return H
+
+
+def gen_matrice_sym(min=2, max=10, pas=5):
+    """
+    Génère une liste de matrice symétrique
+    """
+    list_Msym = []
+    for i in range(min, max, pas):
+        A = np.random.rand(i,i)
+        As = (A + A.T)/2
+        s = matrice_sym(As)
+        if s == True:
+            list_Msym.append(As)
+    return list_Msym
+
+
+def gen_matrice(min=2, max=10, pas=5):
+    """
+    Génère des matrices aléatoires
+    """
+    list_M = []
+    for i in range(min, max, pas):
+        A = np.random.rand(i,i)
+        list_M.append(A)
+    return list_M
+
+
+def affichage_méthode1(A):
+    mpi_A = méthode_puissance_itérée(A)
+    eig_A = np.linalg.eig(A)
+    print("\nMatrice : \n", A)
+    print("\n- valeur propre max de A : ", mpi_A[1], "\n- vecteur propre associé : \n", mpi_A[0], "\n- nombre d'itération : ", mpi_A[2], "\n- erreur : ", mpi_A[3])
+    print("\nDiagonalisation de A avec numpy : ")
+    print("\n- valeur propre de A : \n", eig_A[0], "\n- matrice de passage associée : \n", eig_A[1])
+
+def affichage_norme1(A):
+    norme = norme_euclidienne(A)
+    print("\n- norme calculée à partir de la puissance itérée :\n", norme[0])
+    print("- norme calculée à de la bibliothèque numpy :\n", norme[1])
+
+
+def affichage_méthode2(A):
+    mpi_A = méthode_puissance_inverse(A)
+    eig_A = np.linalg.eig(A)
+    print("\nMatrice : \n", A)
+    print("\n- valeur propre max de A**-1 : ", mpi_A[1],"\n- valeur propre min de A : ", mpi_A[2], "\n- vecteur propre associé : \n", mpi_A[0], "\n- nombre d'itération : ", mpi_A[3], "\n- erreur : ", mpi_A[4])
+    print("\nDiagonalisation de A avec numpy : ")
+    print("\n- valeurs propres de A : \n", eig_A[0], "\n- matrice de passage associée : \n", eig_A[1])
+
+def affichage_cond2(A):
+    cond = conditionnement(A)
+    print("\n- conditionnement calculée à partir de la puissance inverse :\n", cond[0])
+    print("- conditionnement calculée à de la bibliothèque numpy :\n", cond[1])
+
+
+def affichage_méthode3(A):
+    mpi_A = méthode_puissance_itérée2(A)
+    eig_A = np.linalg.eig(A)
+    print("\nMatrice : \n", A)
+    print("\n- valeur propre max de A : ", mpi_A[1], "\n- vecteur propre associé : \n", mpi_A[0], "\n- nombre d'itération : ", mpi_A[3], "\n- erreur : ", mpi_A[2])
+    print("\nDiagonalisation de A avec numpy : ")
+    print("\n- valeurs propres de A : \n", eig_A[0], "\n- matrice de passage associée : \n", eig_A[1])
+
+def affichage_méthode4(A):
+    mpi_A = diag_QR(A)
+    eig_A = np.linalg.eig(A)
+    print("\nMatrice : \n", A)
+    print("\n- matrice triangulaire sup Ak : \n", mpi_A[0], "\n- matrice de passage orthogonale P : \n", mpi_A[1], "\n- nombre d'itération : ", mpi_A[2], "\n- valeur maximale Mk : ", mpi_A[3])
+    print("\nDiagonalisation de A avec numpy : ")
+    print("\n- valeurs propres de A : \n", eig_A[0], "\n- matrice de passage associée : \n", eig_A[1])
